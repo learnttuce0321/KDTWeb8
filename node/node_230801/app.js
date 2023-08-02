03ee9e2f2d0ad0ac81af065f325adc87468da210
learnttuce0321 <lettucegood2@gmail.com>
2023-08-02 13:45:06 +0900

    230802_nodejs

--- node/node_230801/app.js
+++ node/node_230801/app.js
@@ -41,7 +41,38 @@ app.get('/exercise1', (req, res) => {
     res.send(req.query)
 })
 app.post('/exercise2', (req, res) => {
-    res.send(req.body)
+
+    console.log(req.body)
+
+    const ID = req.body.userId
+    const PW = req.body.userPassword
+
+    var checkEmail = !ID.match(/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/)
+    var checkNum = PW.search(/[0-9]/g);
+    var checkEng = PW.search(/[a-z]/ig);
+    var checkSpe = PW.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
+    if (ID.length === 0) {
+        return res.send({ result: false })
+    } else if (checkEmail) {
+        return res.send({ result: false })
+    }
+
+    if (PW.length < 8 || PW.length > 20) {
+        return res.send({ result: false })
+    } else if (PW.search(/\s/) != -1) {
+        return res.send({ result: false })
+    } else if (checkNum < 0 || checkEng < 0 || checkSpe < 0) {
+        return res.send({ result: false })
+    }
+
+    res.send({
+        result: true,
+        userInfo: {
+            userID: ID,
+            userPassword: PW
+        }
+    })
 })
 
 app.listen(PORT, () => {
